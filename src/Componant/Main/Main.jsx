254446d4f9f6e76ba65c4b0e9763a005b4b31ad2
Fady Clever <Cleverfady@gmail.com>
2024-11-19 08:41:44 +0000

    solve all bugs

--- src/Componant/Main/Main.jsx
+++ src/Componant/Main/Main.jsx
@@ -1,160 +1,180 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
+import Card from "react-bootstrap/Card";
+import { Oval } from "react-loader-spinner";
 
+import { Link } from "react-router-dom";
 
-import Card from 'react-bootstrap/Card';
-import { Oval } from 'react-loader-spinner'
+import Style from "./Main.module.css";
+import { Helmet } from "react-helmet";
+import { myProjects } from "../Atoms/ProjectAtom";
+import { useRecoilState } from "recoil";
+import { Fade } from "react-awesome-reveal";
 
-import {Link} from "react-router-dom"
+export function Main() {
+  const [projectLength, setProjectLength] = useRecoilState(myProjects);
+  const [currentActive, setCurrentActive] = useState("all");
+  const [arr, setArr] = useState(projectLength);
 
-import Style from "./Main.module.css" 
-import { Helmet } from 'react-helmet'
-import { myProjects } from '../Atoms/ProjectAtom';
-import { useRecoilState } from 'recoil';
-import { Fade } from 'react-awesome-reveal'; 
-
-export  function Main() {
-
-
-const[projectLength , setProjectLength]=useRecoilState(myProjects)
-const[currentActive , setCurrentActive] = useState("all");
-const[arr , setArr] = useState(projectLength);
-
-console.log(projectLength);
-  return <>
-   <Helmet>
+  console.log(projectLength);
+  return (
+    <>
+      <Helmet>
         <meta name="Main" content="This is Main page to see my projects" />
       </Helmet>
-  
-  <section className={` ${Style.main} mt-4 main mx-auto`}>
-  <div className='row'>
-    <div className= {` ${Style.allBtn} col-md-3  `}>
-      <div className='d-flex  flex-md-column flex-wrap justify-content-center '>
-      <button  
-onClick={()=>{
-  setCurrentActive("all")
-  setArr(projectLength)
-}}
-  className={`${currentActive ==="all" ? Style.active : null} $ btn ms-2 my-2 py-2 px-2 rounded-4`}
-    >All Projects</button>
 
-<button
-onClick={()=>{
-  setCurrentActive("css")
-  const newArr = projectLength.filter((item)=>{
-    return item.category =="css"
-  })
-  setArr(newArr);
-}}
-className={`${currentActive ==="css" ? Style.active : null} $ btn ms-2 my-2 py-2 px-2 rounded-4`}
- 
- > HTML And CSS</button>
-<button 
-onClick={()=>{
-  setCurrentActive("js")
-  const newArr = projectLength.filter((item)=>{
-    return item.category =="js"
-  })
-  setArr(newArr);
-}}
-className={`${currentActive ==="js" ? Style.active : null} $ btn ms-2 my-2 py-2 px-2 rounded-4`}
->JavaScript</button>
-<button 
-onClick={()=>{
-  setCurrentActive("jQuery")
-  const newArr = projectLength.filter((item)=>{
-    return item.category =="jQuery"
-  })
-  setArr(newArr);
-}}
-className={`${currentActive ==="jQuery" ? Style.active : null} $ btn ms-2 my-2 py-2 px-2 rounded-4`}
->jQuery</button>
-<button 
-onClick={()=>{
-  setCurrentActive("react")
-  const newArr = projectLength.filter((item)=>{
-    return item.category =="react"
-  })
-  setArr(newArr);
-}}
-className={`${currentActive ==="react" ? Style.active : null} $ btn ms-2 my-2 py-2 px-2 rounded-4`}
->React.JS</button>
+      <section className={` ${Style.main} mt-4 main mx-auto`}>
+        <div className="row">
+          <div className={` ${Style.allBtn} col-md-3  `}>
+            <div className="d-flex  flex-md-column flex-wrap justify-content-center ">
+              <button
+                onClick={() => {
+                  setCurrentActive("all");
+                  setArr(projectLength);
+                }}
+                className={`${
+                  currentActive === "all" ? Style.active : null
+                } $ btn ms-2 my-2 py-2 px-2 rounded-4`}
+              >
+                All Projects
+              </button>
 
-<button 
-onClick={()=>{
-  setCurrentActive("next")
-  const newArr = projectLength.filter((item)=>{
-    return item.category =="next"
-  })
-  setArr(newArr);
-}}
-className={`${currentActive ==="next" ? Style.active : null} $ btn ms-2 my-2 py-2 px-2 rounded-4`}
->Next JS</button>
+              <button
+                onClick={() => {
+                  setCurrentActive("css");
+                  const newArr = projectLength.filter((item) => {
+                    return item.category == "css";
+                  });
+                  setArr(newArr);
+                }}
+                className={`${
+                  currentActive === "css" ? Style.active : null
+                } $ btn ms-2 my-2 py-2 px-2 rounded-4`}
+              >
+                {" "}
+                HTML And CSS
+              </button>
+              <button
+                onClick={() => {
+                  setCurrentActive("js");
+                  const newArr = projectLength.filter((item) => {
+                    return item.category == "js";
+                  });
+                  setArr(newArr);
+                }}
+                className={`${
+                  currentActive === "js" ? Style.active : null
+                } $ btn ms-2 my-2 py-2 px-2 rounded-4`}
+              >
+                JavaScript
+              </button>
+              <button
+                onClick={() => {
+                  setCurrentActive("jQuery");
+                  const newArr = projectLength.filter((item) => {
+                    return item.category == "jQuery";
+                  });
+                  setArr(newArr);
+                }}
+                className={`${
+                  currentActive === "jQuery" ? Style.active : null
+                } $ btn ms-2 my-2 py-2 px-2 rounded-4`}
+              >
+                jQuery
+              </button>
+              <button
+                onClick={() => {
+                  setCurrentActive("react");
+                  const newArr = projectLength.filter((item) => {
+                    return item.category == "react";
+                  });
+                  setArr(newArr);
+                }}
+                className={`${
+                  currentActive === "react" ? Style.active : null
+                } $ btn ms-2 my-2 py-2 px-2 rounded-4`}
+              >
+                React.JS
+              </button>
 
-
-      </div>
-
-    </div>
-    <div className={`${Style.allCard} col-md-9 `}>
-<div className={` row  ms-md-3 mt-3`}>
-{arr ? arr.map((item)=><>
- 
-  <div className='col-md-6 d-flex  ' >
-  <Fade  direction='right' duration={1000}>
-  <Card className={`${Style.cardStyle} mb-3 `} key={item.id}>
-      <Card.Img variant="top" className={`${Style.imgStyle} `} src={item.images.cover} />
-      <Card.Body className='p-3 '>
-        <Card.Title> 
-          <h2 className=' h4 fw-bolder mb-3'>
-          {item.title}
-          <i className={` ${item.icon} ms-2`} ></i>
-           </h2>
-           </Card.Title>
-        <Card.Text >
-           <p className='fs-6'>
-           {item.description}
-           </p> 
-        </Card.Text>
-        <div className='d-flex justify-content-between align-items-center'>
-          <div className={`${Style.icons} mt-2 d-flex`}>
-          <a href={item.demo} target="_blank">
-          <i className="fa-solid ms-1   fa-link"></i>
-          </a>
-     <a href={item.github} target="_blank">
-     <i class=" ms-2  fa-brands fa-github"></i>
-     </a>
-          
-  
+              <button
+                onClick={() => {
+                  setCurrentActive("next");
+                  const newArr = projectLength.filter((item) => {
+                    return item.category == "next";
+                  });
+                  setArr(newArr);
+                }}
+                className={`${
+                  currentActive === "next" ? Style.active : null
+                } $ btn ms-2 my-2 py-2 px-2 rounded-4`}
+              >
+                Next JS
+              </button>
+            </div>
           </div>
-          <Link className=' text-primary me-2 ' to={`/projectDetails/${item.id}`}>
-            more
-            <i className=" ms-1 fa-solid fa-arrow-right"></i>
-          </Link>
+          <div className={`${Style.allCard} col-md-9 `}>
+            <div className={` row  ms-md-3 mt-3`}>
+              {arr ? (
+                arr.map((item) => (
+                  <div key={item.id} className="col-md-6 d-flex  ">
+                    <Fade direction="right" duration={1000}>
+                      <Card
+                        className={`${Style.cardStyle} mb-3 `}
+                        key={item.id}
+                      >
+                        <Card.Img
+                          variant="top"
+                          className={`${Style.imgStyle} `}
+                          src={item.images.cover}
+                        />
+                        <Card.Body className="p-3 ">
+                          <Card.Title>
+                            <h2 className=" h4 fw-bolder mb-3">
+                              {item.title}
+                              <i className={` ${item.icon} ms-2`}></i>
+                            </h2>
+                          </Card.Title>
+                          <div>
+                            <p className="fs-6">{item.description}</p>
+                          </div>
+                          <div className="d-flex justify-content-between align-items-center">
+                            <div className={`${Style.icons} mt-2 d-flex`}>
+                              <a href={item.demo} target="_blank">
+                                <i className="fa-solid ms-1   fa-link"></i>
+                              </a>
+                              <a href={item.github} target="_blank">
+                                <i className=" ms-2  fa-brands fa-github"></i>
+                              </a>
+                            </div>
+                            <Link
+                              className=" text-primary me-2 "
+                              to={`/projectDetails/${item.id}`}
+                            >
+                              more
+                              <i className=" ms-1 fa-solid fa-arrow-right"></i>
+                            </Link>
+                          </div>
+                        </Card.Body>
+                      </Card>
+                    </Fade>
+                  </div>
+                ))
+              ) : (
+                <Oval
+                  visible={true}
+                  height="25"
+                  width="55"
+                  color="#4fa94d"
+                  ariaLabel="oval-loading"
+                  wrapperStyle={{}}
+                  // wrapperClass=""
+                />
+              )}
+            </div>
+          </div>
         </div>
-      </Card.Body>
-    </Card>
-    </Fade>
-
-   
- 
-
-  </div>
-  
-  </>) : <Oval
-                visible={true}
-                height="25"
-                width="55"
-                color="#4fa94d"
-                ariaLabel="oval-loading"
-                wrapperStyle={{}}
-                wrapperClass=""
-                />}
-</div>
-
-    </div>
-    </div>
-    
-  </section>
-  
-  </>
+      </section>
+    </>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { RecoilRoot } from 'recoil';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { RecoilRoot } from "recoil";
 
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min";
@@ -12,14 +12,11 @@ import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <RecoilRoot>
-<React.StrictMode>
     <App />
-  </React.StrictMode>
   </RecoilRoot>
-  
 );
 
 // If you want to start measuring performance in your app, pass a function
